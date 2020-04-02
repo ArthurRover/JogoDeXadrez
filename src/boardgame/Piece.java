e63cfd8f15f1c5051036b6319da6151e6a44e66c
ArthurRover <arthurssj621@gmail.com>
2020-04-02 19:09:00 -0400

    Starting to implement Borad and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,35 @@
+package boardgame;
+
+public class Piece {
+	protected Position position;
+	private Board board;
+	
+	public Piece() {
+	}
+
+	public Piece(Board board) {
+		this.board = board;
+	}
+
+	public Position getPosition() {
+		return position;
+	}
+
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+	
+	
+	
+	
+	
+	
+}
